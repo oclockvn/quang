0181a06fb6be6575fae1a744a010916f4c42a817
oclockvn <oclockvn@gmail.com>
2016-08-06 12:57:41 +0700

    add components

--- quang/quang.web/App_Start/BundleConfig.cs
+++ quang/quang.web/App_Start/BundleConfig.cs
@@ -20,12 +20,12 @@ namespace quang.web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      //"~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
         }
     }
 }
