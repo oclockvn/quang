85fe4e7f8c3cf9d162f51c15592d511e29f49386
oclockvn <oclockvn@gmail.com>
2016-08-06 12:23:49 +0700

    add source

---
+++ quang/quang.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(quang.web.Startup))]
+namespace quang.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
