860a475429296ec2a423e633f1bb8855d36e7846
oclockvn <oclockvn@gmail.com>
2016-08-09 15:16:35 +0700

    adding post

--- quang/quang.web/Scripts/app/utils.js
+++ quang/quang.web/Scripts/app/utils.js
@@ -1,17 +1,13 @@
-﻿$(document).ready(function () {
-    /*
-    var shine = new Shine(document.getElementById('logo-text'));
-    window.addEventListener('mousemove', function (event) {
-        shine.light.position.x = event.clientX;
-        shine.light.position.y = event.clientY;
-        shine.draw();
-    }, false);
-    */
-
+﻿/*
+$(document).ready(function () {    
     var shine2 = new Shine(document.getElementById('text'));
-    window.addEventListener('mousemove', function (event) {
-        shine2.light.position.x = event.clientX;
-        shine2.light.position.y = event.clientY;
-        shine2.draw();
-    }, false);
+    console.log(shine2);
+    if (shine2 != null) {
+        window.addEventListener('mousemove', function (event) {
+            shine2.light.position.x = event.clientX;
+            shine2.light.position.y = event.clientY;
+            shine2.draw();
+        }, false);
+    }
 });
+*/
