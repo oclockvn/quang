d997871cd7528dbbb580882ee33e648a3ea36b03
oclockvn <oclockvn@gmail.com>
2016-08-09 12:33:18 +0700

    complete index

---
+++ quang/quang.web/Scripts/app/utils.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    /*
+    var shine = new Shine(document.getElementById('logo-text'));
+    window.addEventListener('mousemove', function (event) {
+        shine.light.position.x = event.clientX;
+        shine.light.position.y = event.clientY;
+        shine.draw();
+    }, false);
+    */
+
+    var shine2 = new Shine(document.getElementById('text'));
+    window.addEventListener('mousemove', function (event) {
+        shine2.light.position.x = event.clientX;
+        shine2.light.position.y = event.clientY;
+        shine2.draw();
+    }, false);
+});
