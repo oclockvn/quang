810b4b510788842eced2131bdd8167b49983fa19
oclockvn <oclockvn@gmail.com>
2016-08-26 18:32:01 +0700

    update whatever

--- quang/quang.web/Scripts/app/utils.js
+++ quang/quang.web/Scripts/app/utils.js
@@ -1,8 +1,41 @@
-﻿
+﻿function onToggleSearchSection(e) {
+    e.preventDefault();
+
+    var $btn = $(this),
+        $seachSection = $('#search-section');
+
+    // $seachSection.toggleClass('active');
+    $('body').toggleClass('hide-scroll');
+    $seachSection.fadeToggle();
+}
+
+function onScrollTo(e) {
+    e.preventDefault();
+
+    var $btn = $(this),
+        target = $btn.attr('data-scroll-target'),
+        duration = $btn.attr('data-scroll-duration');
+
+    if (typeof target == 'undefined' || target == null) return;
+    if (typeof duration == 'undefined' || duration == null || typeof duration != 'number')
+        duration = 400;
+
+    if (target.indexOf('#') < 0) {  // scroll to offset
+        $.scrollTo(target, duration);
+    } else {                        // scroll to object
+        var $target = $(target);
+        if ($target.length == 0) // target object not found
+            return;
+
+        $.scrollTo($target, duration);
+    }
+}
+
+
 $(document).ready(function () {
 
-    var shineTarget = document.getElementById('text');
-    console.log(shineTarget);
+    // shine
+    var shineTarget = document.getElementById('text');    
     if (shineTarget != null) {
         var shine2 = new Shine(shineTarget);
         window.addEventListener('mousemove', function (event) {
@@ -11,4 +44,10 @@ $(document).ready(function () {
             shine2.draw();
         }, false);
     }
+
+    // scroll
+    $(document).on('click', '.btn-scroll', onScrollTo);
+
+    // toggle search
+    $(document).on('click', '.btn-toggle-search', onToggleSearchSection);
 });
