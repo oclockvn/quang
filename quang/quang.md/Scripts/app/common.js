68c5659fc15ed2d7a2b62ae31bb9b82dc3265f02
oclockvn <oclockvn@gmail.com>
2016-08-31 01:09:12 +0700

    update index view

--- quang/quang.md/Scripts/app/common.js
+++ quang/quang.md/Scripts/app/common.js
@@ -1,12 +1,21 @@
-﻿function onToggleSearchSection(e) {
+﻿var previousOffset = 0,
+    scrollTop = 0,
+    navOffset = 0,
+    $nav = null,
+    $win = null,
+    $btnScrollTop = null,
+    $header = null;
+
+
+function onToggleSearchClicked(e) {
     e.preventDefault();
 
     var $btn = $(this),
-        $seachSection = $('#search-section');
+        $seachSection = $('.header__search');
 
     // $seachSection.toggleClass('active');
-    $('body').toggleClass('hide-scroll');
-    $seachSection.fadeToggle();
+    //$('body').toggleClass('hide-scroll');
+    $seachSection.toggleClass('active');
 }
 
 function onScrollTo(e) {
@@ -31,14 +40,79 @@ function onScrollTo(e) {
     }
 }
 
+function onWindowScroll() {
+
+    scrollTop = $win.scrollTop();
+
+    // fixed nav
+    if (scrollTop >= navOffset) {
+        (!$header.is('.fixed')) && $header.addClass('fixed');
+        (!$nav.is('.down')) && $nav.addClass('down');
+    } else {
+        ($header.is('.fixed')) && $header.removeClass('fixed');
+        ($nav.is('.down')) && $nav.removeClass('down');
+    }
+
+    // scroll button
+    if (previousOffset > scrollTop) { // scroll up
+        // $btnScrollTop.removeClass('down');
+        if ($btnScrollTop.is('.down'))
+            $btnScrollTop.removeClass('down');
+    } else { // scroll down
+        if (!$btnScrollTop.is('.down'))
+            $btnScrollTop.addClass('down');
+    }
+    //console.log('previous = %s, scrollTop = %s', previousOffset, scrollTop);
+    previousOffset = scrollTop;
+}
+
+function onScrollButtonClicked(e) {
+    e.preventDefault();
+
+    var $btn = $(this);
+    if ($btn.is('.down')) {
+        $.scrollTo($('#footer'), 400, function () {
+            // after scroll to top -> allow scroll down
+            console.log('bottom callback');
+            setTimeout(function () {
+                $btn.removeClass('down');
+            }, 100);
+            
+        });
+    } else {
+        $.scrollTo(0, 400, function () {
+            // after scroll to bottom -> allow scroll up
+            console.log('top callback');
+            setTimeout(function () {
+                $btn.addClass('down');
+            }, 100);
+            
+        });
+    }
+}
 
 $(document).ready(function () {
-    // scroll
+
+    $win = $(window);
+    $btnScrollTop = $('.btn-scroll-top');
+    $nav = $('.header__nav');
+    $header = $('.header');
+    navOffset = $nav.offset().top;
+    
+
+    // scroll button
     $(document).on('click', '.btn-scroll', onScrollTo);
 
+    // scroll top or button
+    $(document).on('click', '.btn-scroll-top', onScrollButtonClicked);
+
+    // document scroll
+    $(window).on('scroll', onWindowScroll);
+
     // toggle search
-    // $(document).on('click', '.btn-toggle-search', onToggleSearchSection);
+    $(document).on('click', '.btn-toggle-search', onToggleSearchClicked);
 
     // fix nav
-    // $('#nav').sticky({ topSpacing: 0 });
+    //$('.nav--right').sticky({ topSpacing: 0 });
+    $('.category').sticky({ topSpacing: 60 });
 });
